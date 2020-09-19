78b7327d3a8b2ff2aced19de0cb52be91783a8a3
ShoeraRei <70783940+ShoeraRei@users.noreply.github.com>
2020-09-19 20:45:08 +0700

    Add files via upload
    Version 1

---
+++ Nuclear_code_checker.py
@@ -0,0 +1,33 @@
+import requests;
+from bs4 import BeautifulSoup;
+
+print("\nwe are not responsible if there are other parties who aim to misuse the nuclear code\n");
+launch_code = input("enter the launch code :");
+print('please wait checking code:'+launch_code+"....");
+code_link =["https://nhentai.net/g/"+launch_code+"/","https://www.tsumino.com/entry/"+launch_code,"https://hentaifox.com/gallery/"+launch_code+"/","https://imhentai.com/gallery/"+launch_code+"/","https://asmhentai.com/g/"+launch_code+"/","https://hentai.cafe/hc.fyi/"+launch_code,"http://www.hbrowse.com/"+launch_code+"/c00001"];
+q = 0;
+t = 1;
+list_link = [];
+list_title = [];
+while q < len(code_link):
+  code_request = requests.get(code_link[q]);
+  code_soup = BeautifulSoup(code_request.content,"html.parser");
+  if code_request.status_code == 200:
+    title = code_soup.title.text;
+    list_link.append(code_link[q]);
+    list_title.append(title);
+  else:
+    print(str(t)+" nuclear server has encountered an error or use vpn");
+    t+=1;
+  q+=1;
+  
+if list_link == None:
+  print("your input wrong code");
+else:
+  e = 0;
+  for w in list_title:
+    print("\n"+str(e)+". "+list_title[e]);
+    e+= 1;
+  selected = int(input("\nselect : "))
+  print("\nnuclear link : "+list_link[selected]);
+  print("use the nuclear code wisely");
